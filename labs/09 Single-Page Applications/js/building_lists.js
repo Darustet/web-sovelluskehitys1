25087eba1bcef27798f73a28afedbb5e24c68f20
Darustet <58421506+Darustet@users.noreply.github.com>
2021-04-07 04:53:26 +0300

    lab9 2.2 kesken

--- labs/09 Single-Page Applications/js/building_lists.js
+++ labs/09 Single-Page Applications/js/building_lists.js
@@ -7,11 +7,13 @@ console.log(data);
 
 var books = data.books;
 
-var list = document.createElement('ol');
+var list = document.createElement('TABLE');
 for (var i=0; i < books.length; i++) {
 	console.log(books[i].title);
-	var item = document.createElement('li');
-	item.innerHTML = books[i].title;
+	var item = list.insertRow(i);
+	item.innerHTML = books[i];
+
 	list.appendChild(item);
 }
 document.body.appendChild(list);
+
--- labs/09 Single-Page Applications/js/simple_dom.js
+++ labs/09 Single-Page Applications/js/simple_dom.js
@@ -2,23 +2,29 @@
 
 console.log(document);
 
-document.getElementById('save').onclick = save;
-document.querySelector('#userForm input[type="email"]').onkeypress = function() {
-	console.log('updating email');
-	var email = document.querySelector('#userForm input[type="email"]').value;
-	document.querySelector('#summary p').innerHTML = email;
-};
+const input = document.querySelector('#userForm');
 
+input.addEventListener('input', updateValue);
 
-function save() {
+	function updateValue(e) {
 	console.log('save');
 	// get a DOM object representing a form field.
 	var name = document.querySelector('#userForm input[type="text"]');
 	console.log(name);
 	document.querySelector('#summary h1').innerHTML = name.value;
+	console.log('updating email');
+	var email = document.querySelector('#userForm input[type="email"]').value;
+	document.querySelector('#summary p').innerHTML = email;
 	var data = document.querySelectorAll('#userForm input');
 	console.log(data);
 	var paragraphs = document.querySelectorAll('#summary p');
 	console.log('found '+paragraphs.length+' p tags');
-	paragraphs[1].innerHTML = 'Hello World!';
+	var password = document.querySelector('#userForm input[type="password"]').value;
+	paragraphs[1].innerHTML = password;
+	//paragraphs[1].innerHTML = 'Hello World!';
 }
+document.querySelector('#userForm input[type="email"]').onclick = function() {
+	console.log('updating email');
+	var email = document.querySelector('#userForm input[type="email"]').value;
+	document.querySelector('#summary p').innerHTML = email;
+};
